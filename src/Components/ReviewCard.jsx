7eb2c0b806a88a48e2f498b1535956ac13c48025
cherrydub <cherrydub@gmail.com>
2023-04-18 12:13:56 +0100

    added an expand button, this way I"ll either provide more info or all the info that should be presented in singleReview, will leave open for now and get back to it once seems more appropriate

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import ReviewSingle from "./ReviewSingle";
 
 export default function ReviewCard({
   owner,
@@ -11,6 +13,12 @@ export default function ReviewCard({
   designer,
   comment_count,
 }) {
+  const [showReview, setShowReview] = useState(false);
+
+  const handleExpand = () => {
+    showReview ? setShowReview(false) : setShowReview(true);
+  };
+
   return (
     <div className="ReviewCard border-black border-2 max-w-xs">
       <img src={review_img_url} alt={title} />
@@ -26,6 +34,29 @@ export default function ReviewCard({
             </button>
           </Link>
         </li>
+
+        {showReview ? (
+          <div>
+            More info will go here soon
+            <p></p>
+            .....
+            <p></p>
+            <button
+              className="text-black opacity-50 px-4 py-2 hover:opacity-100 bg-red-300"
+              onClick={handleExpand}
+            >
+              go back
+            </button>
+            {/* Additional content to show when "show more" is clicked */}
+          </div>
+        ) : (
+          <button
+            className="rounded-full text-black opacity-50 px-4 py-2 hover:opacity-100 bg-blue-300"
+            onClick={handleExpand}
+          >
+            expand
+          </button>
+        )}
         {/* 
         <li>votes: {votes}</li>
         <li>designer: {designer}</li>
