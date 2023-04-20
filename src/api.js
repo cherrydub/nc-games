b2b12d972fc4012481f195b8ba9343c203a9321c
cherrydub <cherrydub@gmail.com>
2023-04-20 14:02:57 +0100

    swapped all api to await/async

--- src/api.js
+++ src/api.js
@@ -28,28 +28,22 @@ export const postComment = async (review_id, username, body) => {
   return res.data.comment;
 };
 
-export const getReviewId = (review_id) => {
-  return baseAPI.get(`/reviews/${review_id}`).then((res) => {
-    return res.data.review;
-  });
+export const getReviewId = async (review_id) => {
+  const res = await baseAPI.get(`/reviews/${review_id}`);
+  return res.data.review;
 };
 
-export const getReviewIdcomments = (review_id) => {
-  return baseAPI.get(`/reviews/${review_id}/comments`).then((res) => {
-    return res.data.comments;
-  });
+export const getReviewIdcomments = async (review_id) => {
+  const res = await baseAPI.get(`/reviews/${review_id}/comments`);
+  return res.data.comments;
 };
 
-export const getCategories = () => {
-  return baseAPI.get("/categories").then((res) => {
-    return res.data.category;
-  });
+export const getCategories = async () => {
+  const res = await baseAPI.get("/categories");
+  return res.data.category;
 };
 
-export const patchVotes = (review_id) => {
-  return baseAPI
-    .patch(`/reviews/${review_id}`, { inc_votes: 1 })
-    .then((res) => {
-      return res.data.review;
-    });
+export const patchVotes = async (review_id) => {
+  const res = await baseAPI.patch(`/reviews/${review_id}`, { inc_votes: 1 });
+  return res.data.review;
 };
