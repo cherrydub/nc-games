612f8ff9d08e06ba6acfed6cea78c28b50ea961a
cherrydub <cherrydub@gmail.com>
2023-06-15 16:45:34 +0100

    switched back the ternary after testing

--- src/Components/HomeBody.jsx
+++ src/Components/HomeBody.jsx
@@ -6,11 +6,11 @@ export default function HomeBody({ redirectReviews }) {
     <div className="bg-white bg-opacity-50 p-4">
       <div className="">
         Welcome to NC Games,{" "}
-        {!redirectReviews
+        {redirectReviews
           ? "all files loaded"
           : "please allow a bit of time for the Reviews to be fetched"}
       </div>
-      {!redirectReviews ? (
+      {redirectReviews ? (
         <div>
           <Link to={`/reviews`} key={crypto.randomUUID()}>
             <button className="border  rounded-full text-black hover:text-orange-400 opacity-100 px-4 py-2 hover:bg-grey-400 bg-white">
